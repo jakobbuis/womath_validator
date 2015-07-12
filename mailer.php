c454a42a794d822bf64455b69e90a87d82360e6f
Jakob Buis <jakob@cubiso.nl>
2015-07-12 22:59:49 +0200

    fixes to email validator

--- mailer.php
+++ mailer.php
@@ -1,18 +1,22 @@
 <?php
 
 require 'vendor/autoload.php';
+require 'config.php';
 
 /*
  * Setup
  */
 $database = new medoo($config['database']);
 $mandrill = new Mandrill($config['mandrill']['api_key']);
-$twig = new Twig();
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader);
+$emailHTML = $twig->loadTemplate('email.html.twig');
+$emailText = $twig->loadTemplate('email.text.twig');
 
 /*
  * Gather all entries to sent
  */
-$entries = $database->select('people', ['uuid', 'name', 'email', 'company_name'], ['company_name[!]' => null]);
+$entries = $database->select('people', ['uuid', 'name', 'email', 'company_name'], ['company_name[!]' => null, 'GROUP' => 'email']);
 
 /*
  * Queue all emails
@@ -21,22 +25,24 @@ foreach ($entries as $entry) {
 
     // Compose message
     $data = [
-        'company' => $entry['uuid'],
-        'uuid'    => $entry['company_name'],
+        'company' => $entry['company_name'],
+        'uuid'    => $entry['uuid'],
         'name'    => $entry['name']
     ];
     $message = [
-        'html'       => $twig->render('email.html.twig', $data),
-        'text'       => $twig->render('email.text.twig', $data),
-        'subject'    => 'Research project Eclipse ecosystem: confirmation of your employer is requested',
-        'from_email' => 'jakob@jakobbuis.nl',
-        'from_name'  => 'Jakob Buis',
-        'to'         => [['email' => $entry['email']]]
+        'html'         => $emailHTML->render($data),
+        'text'         => $emailText->render($data),
+        'subject'      => 'Research project Eclipse ecosystem: confirmation of your employer is requested',
+        'from_email'   => 'jakob@jakobbuis.nl',
+        'from_name'    => 'Jakob Buis',
+        'to'           => [['email' => $entry['email']]],
+        'track_opens'  => false,
+        'track_clicks' => false
     ];
 
     // Send email
-    $result = $mandrill->messages->send($message, false, 'Main pool', 'example send_at');
+    $result = $mandrill->messages->send($message, false);
 
     // Return result
-    echo 'Email sent to ID ' . $entry['uuid'] . ' email ' . $entry['email'] . ' status ' . $result['status'] . "\n";
+    echo 'Email sent to ID ' . $entry['uuid'] . ' email ' . $entry['email'] . ' status ' . $result[0]['status'] . "\n";
 }
